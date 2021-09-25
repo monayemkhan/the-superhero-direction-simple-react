6e8058bf8422a02db58bd9945089e31fa27fbde6
Monayem Khan <monaamkhanet@gmail.com>
2021-09-26 01:52:53 +0600

    first commit

---
+++ src/components/Header/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => {
+    return (
+        <div className="header">
+            <h1>Qatar World Cup Opening Ceremony 2022</h1>
+            <h3>Part of the Concert</h3>
+            <p></p>
+            <h2>Total Budget: $100 million</h2>
+        </div>
+    );
+};
+
+export default Header;
